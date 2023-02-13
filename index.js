6f54af0cf1d722602210d78f20125f5fedf5ac67
Jenny-thoughti <124593179+Jenny-thoughti@users.noreply.github.com>
2023-02-13 09:57:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
+// const dotenv = require('dotenv');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const app = express();
-const port = 3000;
+)
 
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json());
 
-//Task 1
-app.get('/hello',(req, ress) => {
-    ress.send('Hello World!');
-})
 
-app.listen(port, () => {
-    console.log('App Listening on port', port)
-})
-
-//MySQL
+// //MySQL
 const pool = mysql.createPool({
     connectionLimit : 10,
     host            : 'localhost',
@@ -25,118 +16,12 @@ const pool = mysql.createPool({
     database        : 'sample'
 })
 
-//Users
-//Read-get all users
-app.get('/users', (req, res) => {
-    pool.getConnection((err, connection) => {
-        if(err) throw err
-        console.log('connected as id ${connection.threaded}')
-
-        connection.query('SELECT * from users', (err, rows) => {
-            connection.release()
-
-            if(!err){
-                res.send(rows)
-            }else{
-                console.log(err)
-            }
-        })
-    })
-})
-
-
-//Read-get a users by id
-app.get('/:id', (req, res) => {
-    pool.getConnection((err, connection) => {
-        if(err) throw err
-        console.log('connected as id ${connection.threaded}')
-
-        connection.query('SELECT * from users WHERE ID = ? ', [req.params.id], (err, rows) => {
-            connection.release()
-
-            if(!err){
-                res.send(rows)
-            }else{
-                console.log(err)
-            }
-        })
-    })
-})
-
-//DELETE users
-app.delete('/:id', (req, res) => {
-    pool.getConnection((err, connection) => {
-        if(err) throw err
-        console.log('connected as id ${connection.threaded}')
-
-        connection.query('Delete from users WHERE ID = ? ', [req.params.id], (err, rows) => {
-            connection.release()
-
-            if(!err){
-                res.send('Users with the Record Id: ${[req.params.id]} has been removed')
-            }else{
-                console.log(err)
-            }
-        })
-    })
-})
-
-
-//Create-Add a users Records 
-app.post('', (req, res) => {
-
-    pool.getConnection((err, connection) => {
-        if(err) throw err
-        console.log('connected as id ${connection.threadId}')
-
-        const params = req.body
-
-        connection.query('INSERT INTO users SET ?', params, (err, rows) => {
-            connection.release()
-
-            if(!err){
-                res.send('Users with the name: ${params.name} has been added.')
-            }else{
-                console.log(err)
-            }
-        })
-
-        console.log(req.body)
-    })
-})
-
-//Update
-app.put('', (req, res) => {
-
-    pool.getConnection((err, connection) => {
-        if(err) throw err
-        console.log('connected as id ${connection.threadId}')
-
-        
-        const {id, name, qualification, profile} = req.body
-
-        connection.query('UPDATE users SET name = ?, qualification = ?, profile = ? WHERE id = ?', [name, qualification,profile, id] , (err, rows) => {
-            connection.release()
-
-            if(!err){
-                res.send('Users with the name : ${name} has been added.')
-            }else{
-                console.log(err)
-            }
-        })
-
-        console.log(req.body)
-    })
-})
 
 
 
-
-
-
-//Posts
-//Read-get all users
-app.get('', (req, res) => {
+// //Posts
+// //Read-get all users
+app.get('/posts', (req, res) => {
     pool.getConnection((err, connection) => {
         if(err) throw err
         console.log('connected as id ${connection.threaded}')
@@ -154,7 +39,7 @@ app.get('', (req, res) => {
 })
 
 
-//Read
+// //Read
 app.get('/posts/:id', (req, res) => {
     pool.getConnection((err, connection) => {
         if(err) throw err
@@ -173,31 +58,30 @@ app.get('/posts/:id', (req, res) => {
 })
 
 
-//create- add new row 
-app.post('/posts', (req, res) => {
+// //create- add new row 
+// app.post('/posts', (req, res) => {
 
-    pool.getConnection((err, connection) => {
-        if(err) throw err
-        console.log('connected as id ${connection.threadId}')
+//     pool.getConnection((err, connection) => {
+//         if(err) throw err
 
-        const postParams = req.body
+//         const postParams = req.body
 
-        connection.query('INSERT INTO posts SET ?', postParams, (err, rows) => {
-            connection.release()
+//         connection.query('INSERT INTO posts SET ?', postParams, (err, rows) => {
+//             connection.release()
 
-            if(!err){
-                res.send('Users with the name: ${postParams.name} has been added.')
-            }else{
-                console.log(err)
-            }
-        })
+//             if(!err){
+//                 res.send('Users with the name: ${postParams.name} has been added.')
+//             }else{
+//                 console.log(err)
+//             }
+//         })
 
-        console.log(req.body)
-    })
-})
+//         console.log(req.body)
+//     })
+// })
 
 
-//Delete
+// //Delete
 app.delete('/posts/:id', (req, res) => {
     pool.getConnection((err, connection) => {
         if(err) throw err
@@ -216,28 +100,69 @@ app.delete('/posts/:id', (req, res) => {
 })
 
 
-//update
-app.put('/posts', (req, res) => {
-    pool.getConnection((err, connection) => {
-        if(err) throw err
-        console.log('connected as id ${connection.threadId}')
+// // //update
+// app.put('/posts', (req, res) => {
+//     pool.getConnection((err, connection) => {
+//         if(err) throw err
+//         console.log('connected as id ${connection.threadId}')
 
         
-        const {id, name, comment_status} = req.body
+//         const {id, name, comment_status} = req.body
 
-        connection.query('UPDATE posts SET name = ?, comment_status = ? WHERE id = ?', [name, comment_status, id] , (err, rows) => {
-            connection.release()
+//         connection.query('UPDATE posts SET name = ?, comment_status = ? WHERE id = ?', [name, comment_status, id] , (err, rows) => {
+//             connection.release()
 
-            if(!err){
-                res.send('Users with the name : ${name} has been added.')
-            }else{
-                console.log(err)
-            }
-        })
+//             if(!err){
+//                 res.send('Users with the name : ${name} has been added.')
+//             }else{
+//                 console.log(err)
+//             }
+//         })
 
-        console.log(req.body)
-    })
-})
+//         console.log(req.body)
+//     })
+// })
 
 
+//const express = require('express');
+//const bodyParser = require('body-parser');
+// import users routes
+const usersRoutes = require('./src/users-route');
+//const postsRoutes = require('./src/posts-route')
+
+
+// create express app
+//const app = express();
+
+// setup the server port
+const port = process.env.PORT || 3000;
+
+// parse request data content type application/x-www-form-rulencoded
+app.use(bodyParser.urlencoded({extended: false}));
+
+// parse request data content type application/json
+app.use(bodyParser.json());
+
+// define root route
+app.get('/hello', (req, res)=>{
+    res.send('Hello World');
+});
+
+
+
+// create users routes
+app.use('/', usersRoutes);
+
+// create posts routes
+//app.use('/', postsRoutes);
+
+
+
+
+
+
+// listen to the port
+app.listen(port, ()=>{
+    console.log(`Express is running at port ${port}`);
+});
 
